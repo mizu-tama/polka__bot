c4bb2a5a46c403f7ed2eea65a8da9b2a4198b378
mizu-tama <tamaki.mizuno@gmail.com>
2019-01-12 00:03:00 +0900

    前回のコミット（c911bb72b891fef80aa73e9c53338052f2ac6c4e）で意図せずポート番号のデフォルト値を変更してしまったのでもとに戻した

--- reply.py
+++ reply.py
@@ -38,5 +38,5 @@ def echo():
     }
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 500))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0',port=port,debug=True)
